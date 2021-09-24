1f8b5784e898ff8b146b2736647ed3690cae0b36
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-09-24 11:43:28 -0400

    help

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+  {
+    question: "Which driver do you prefer?",
+    choice1: "Michael Schumacher",
+    choice2: "Lewis Hamilton",
+    answer: 1,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 1,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+  {
+    question: "Which driver do you prefer?",
+    choice1: "",
+    choice2: "",
+    answer: 2,
+  },
+];
